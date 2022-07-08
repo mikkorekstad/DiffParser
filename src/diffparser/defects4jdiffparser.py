ba8662da5ae221d9a3a936b250d0fe3eeeb60305
mikkorekstad <mikkojohan7@gmail.com>
2022-07-08 16:02:11 +0200

    Add statistics

--- src/diffparser/defects4jdiffparser.py
+++ src/diffparser/defects4jdiffparser.py
@@ -24,6 +24,7 @@ class Defects4jDiffParser(object):
         self.n_bugs = len(self.data)
         self.last_line = False
         self.code_started = False
+        self.statistics = {}
         # Log some info
         logging.info(f"The data contains {self.n_bugs} bugs!")
 
@@ -55,8 +56,13 @@ class Defects4jDiffParser(object):
         -------   
         None                                 
         """
+        # Create list of filenames
         list_of_files = list(commit['changedFiles'].keys())
         print(f"Got a list of {len(list_of_files)} filenames: {list_of_files}")
+        # Add entry in statistics dict
+        self.statistics[commit['revisionId']] = {}
+
+        # Create dict with code mapped to filenames
         sep_dict_files = self.sep_by_files(commit['diff'], list_of_files)
 
         # Iterate through sep_dict_files: key = 'filename', value = ['lines of code']
@@ -66,6 +72,13 @@ class Defects4jDiffParser(object):
             # Add the parsed code to correct location, as described above in docstring.
             commit['changedFiles'][key]['buggyCode'] = sep_dict_snippets['buggyCode']
             commit['changedFiles'][key]['patchedCode'] = sep_dict_snippets['patchedCode']
+
+            # Add some stats
+
+            stats = {'numBuggySnippets': len(sep_dict_snippets['buggyCode']),
+                     'numPatchedSnippets': len(sep_dict_snippets['patchedCode'])}
+
+            self.statistics[commit['revisionId']][key] = stats  # specific_stats
 
     def sep_by_snippets(self, un_separated):
         """
@@ -98,7 +111,6 @@ class Defects4jDiffParser(object):
             else:
                 buggy[current_snippet].append(" " + line[1:])
                 patched[current_snippet].append(" " + line[1:])
-
         return sep_dict
 
     def sep_by_files(self, diff, list_of_files):
@@ -114,19 +126,32 @@ class Defects4jDiffParser(object):
         return sep_dict
 
     def get_file_start_indices(self, diff, list_of_files):
+
+        # Create a dict with filenames as entries.
         file_info = {file_name: {} for file_name in list_of_files}
 
+        # Iterate over each line in the diff
         for i, line in enumerate(diff.splitlines()):
+            # Check if the line is describing subtraction or addition in a file
             if '--- a' in line or '+++ b' in line:
+                # Select the current file from the list_of_files, based on the characters of the line.
                 current_file = [fn for fn in list_of_files if fn in line][0]
+
+                # Store the information about where we found this line
                 file_info[current_file]['firstDescribed'] = file_info[current_file].get('firstDescribed', i)
                 file_info[current_file]['lastDescribed'] = i
 
+        # Iterate over all except the last entries in the file_info dict
         for i, key in enumerate(list(file_info.keys())[:-1]):
+            # Store information about what the next file is
             proceeding = list(file_info.keys())[i+1]
             file_info[key]['proceedingFile'] = proceeding
+            # With information about the next line, we can save the index of this snippets final line.
             file_info[key]['lastLine'] = file_info[proceeding]['firstDescribed'] - 1
+        # We can also save information about the index of the last line for the final snippet:
         file_info[list(file_info.keys())[-1]]['lastLine'] = len(diff.splitlines()) - 1
+
+        # Return the information gathered
         return file_info
 
     def save_to_json(self, file_name):
