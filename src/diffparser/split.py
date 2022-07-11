0009d06e474a0396829207e67497396bb400dda2
mikkorekstad <mikkojohan7@gmail.com>
2022-07-11 14:55:41 +0200

    Implement test train validation split

--- src/diffparser/split.py
+++ src/diffparser/split.py
@@ -1,6 +1,7 @@
 
 import json
 from numpy import cumsum
+from sklearn.model_selection import train_test_split
 
 
 class JsonSplitter(object):
@@ -50,18 +51,19 @@ class JsonSplitter(object):
         buggy_names = [f'{output_name}_src_{extension}.txt' for extension in ['train', 'val', 'test']]
         patched_names = [f'{output_name}_trgt_{extension}.txt' for extension in ['train', 'val', 'test']]
 
+        train_size, val_size, test_size = split_size
+        src_train, src_test, trgt_train, trgt_test = train_test_split(buggy_lst, patched_lst, test_size=test_size,
+                                                                      train_size=train_size + val_size)
+        src_train, src_val, trgt_train, trgt_val = train_test_split(src_train, trgt_train, test_size=val_size,
+                                                                    train_size=train_size)
+
         # Save to text file
         # [self.save_to_txt(split, file_name) for split, file_name in zip(buggy_splits, buggy_names)]
         # print(buggy)
-        self.save_to_txt(buggy_lst, buggy_names[0])
-
-    @staticmethod
-    def percentage_split(seq, percentages):
-        # cdf = cumsum(percentages)
-        # assert cdf[-1] == 1.0
-        # n = len(seq)
-        # return [seq[a:b] for a, b in ]
-        pass
+        # self.save_to_txt(src_train, buggy_names[0])
+        buggy_list = [src_train, src_val, src_test]
+        patched_list = [trgt_train, trgt_val, trgt_test]
+        [self.save_to_txt(split, name) for split, name in zip(buggy_list + patched_list, buggy_names + patched_names)]
 
     @staticmethod
     def save_to_txt(split, file_name):
@@ -81,4 +83,4 @@ class JsonSplitter(object):
 
 if __name__ == '__main__':
     splitter = JsonSplitter('../../output/testExperimental.json')
-    splitter.test_train_val_split(split_size=(0.8, 0.1, 0.1), output_name='../../output/MYTEST', remove_multi_file=True, remove_multi_snippets_per_file=True)
+    splitter.test_train_val_split(split_size=(0.1, 0.8, 0.1), output_name='../../output/MYTEST', remove_multi_file=True, remove_multi_snippets_per_file=True)
