70b6a0215d1a51649bb670d33296e1afb43ed548
mikkorekstad <mikkojohan7@gmail.com>
2022-07-11 14:59:05 +0200

    Documentation

--- src/diffparser/split.py
+++ src/diffparser/split.py
@@ -12,6 +12,19 @@ class JsonSplitter(object):
         self.data = self.read_file(file_name)
 
     def test_train_val_split(self, split_size, output_name, remove_multi_snippets_per_file=False, remove_multi_file=False):
+        """
+
+        Parameters
+        ----------
+        split_size tuple (train size, validation size, test size)
+        output_name str path/name of output file without extension
+        remove_multi_snippets_per_file bool
+        remove_multi_file bool
+
+        Returns
+        -------
+
+        """
 
         buggy_lst = []
         patched_lst = []
@@ -36,33 +49,27 @@ class JsonSplitter(object):
             # Iterate over the files and then snippets of code
             for file in commit['changedFiles'].values():
                 for buggySnippet, patchedSnippet in zip(file['buggyCode'], file['patchedCode']):
-                    print(f'{buggySnippet = }')
                     buggy.append(buggySnippet)
                     patched.append(patchedSnippet)
 
             buggy_lst.append(buggy)
             patched_lst.append(patched)
 
-        # Separate data
-        # buggy_splits = self.percentage_split(buggy_lst, split_size)
-        # patched_splits = self.percentage_split(patched, split_size)
-
         # Create filenames
         buggy_names = [f'{output_name}_src_{extension}.txt' for extension in ['train', 'val', 'test']]
         patched_names = [f'{output_name}_trgt_{extension}.txt' for extension in ['train', 'val', 'test']]
 
+        # Create splits
         train_size, val_size, test_size = split_size
         src_train, src_test, trgt_train, trgt_test = train_test_split(buggy_lst, patched_lst, test_size=test_size,
                                                                       train_size=train_size + val_size)
         src_train, src_val, trgt_train, trgt_val = train_test_split(src_train, trgt_train, test_size=val_size,
                                                                     train_size=train_size)
 
-        # Save to text file
-        # [self.save_to_txt(split, file_name) for split, file_name in zip(buggy_splits, buggy_names)]
-        # print(buggy)
-        # self.save_to_txt(src_train, buggy_names[0])
+        # Nest together the names for easier iteration
         buggy_list = [src_train, src_val, src_test]
         patched_list = [trgt_train, trgt_val, trgt_test]
+        # Save the data to txt files
         [self.save_to_txt(split, name) for split, name in zip(buggy_list + patched_list, buggy_names + patched_names)]
 
     @staticmethod
@@ -83,4 +90,5 @@ class JsonSplitter(object):
 
 if __name__ == '__main__':
     splitter = JsonSplitter('../../output/testExperimental.json')
-    splitter.test_train_val_split(split_size=(0.1, 0.8, 0.1), output_name='../../output/MYTEST', remove_multi_file=True, remove_multi_snippets_per_file=True)
+    splitter.test_train_val_split(split_size=(0.8, 0.1, 0.1), output_name='../../output/MYTEST', remove_multi_file=True,
+                                  remove_multi_snippets_per_file=True)
